7b7e213b1f2dcccd3c669bdc4ec324702bce30bc
Matheus Gabriel <math.gsilva07@gmail.com>
2024-05-13 21:41:02 -0300

    add - create motor configuration

---
+++ Dumbo/config_motors.ino
@@ -0,0 +1,52 @@
+/* Configurações dos estados que os motores podem apresentar durante a 
+  execução do run()
+*/
+
+void setupMotors(int state) {
+  if (state == 0) {
+    motor_direita_frente.off();
+    motor_direta_fundo.off();
+    motor_esquerda_frente.off();
+    motor_esquerda_fundo.off();
+  } else {
+    motor_direita_frente.on();
+    motor_direta_fundo.on();
+    motor_esquerda_frente.on();
+    motor_esquerda_fundo.on();
+  }
+}
+
+void andar_frente(int move) {
+  motor_direita_frente.move(move);
+  motor_direta_fundo.move(move);
+  motor_esquerda_frente.move(move);
+  motor_esquerda_fundo.move(move);
+}
+
+void reverso(int move) {
+  motor_direita_frente.move(-move);
+  motor_direta_fundo.move(-move);
+  motor_esquerda_frente.move(-move);
+  motor_esquerda_fundo.move(-move);
+}
+
+void frente() {
+  motor_direita_frente.move(0);
+  motor_direta_fundo.move(0);
+  motor_esquerda_frente.move(0);
+  motor_esquerda_fundo.move(0);
+}
+
+void curva_esquerda(int move) {
+  motor_direita_frente.move(0);
+  motor_direta_fundo.move(0);
+  motor_esquerda_frente.move(move);
+  motor_esquerda_fundo.move(move);
+}
+
+void curva_direita(int move) {
+  motor_direita_frente.move(move);
+  motor_direta_fundo.move(move);
+  motor_esquerda_frente.move(0);
+  motor_esquerda_fundo.move(0);
+}
